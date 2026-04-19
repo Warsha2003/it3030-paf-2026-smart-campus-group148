2c30b92660f9721f1510d9d07970a0b464412ab8
Rashmika Wickramarathna <it23794184@my.sliit.lk>
2026-04-20 00:26:03 +0530

    Delete backend directory

--- backend/src/main/java/com/group148/smartcampus/repository/resource/ResourceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.group148.smartcampus.repository.resource;
-
-import com.group148.smartcampus.entity.Resource;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ResourceRepository extends MongoRepository<Resource, Long> {
-}
